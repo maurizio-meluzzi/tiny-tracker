f03a5ee66bb1b740b70c123d6f04d818f3b3aa71
Maurizio Meluzzi <maurizio@nuvolagroup.com.au>
2023-03-13 01:08:32 +0100

    first raw example of main widget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ActivityWidgetComponent } from './activity-widget/activity-widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityWidgetComponent
   ],
   imports: [
     BrowserModule
